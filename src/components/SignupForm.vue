9fa3cff31b0eef2d63b9640e3ddf3c1fe41b856e
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-13 12:11:06 +0100

    Updated SignupForm.vue

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -92,4 +92,23 @@ input[type="checkbox"] {
     position: relative;
     top: 2px;
 }
+
+.pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
+    transition: 200ms all ease-in-out;
+}
+
+.pill:hover {
+    background: #dad6d6;
+    color: #504e4e;
+}
 </style>
