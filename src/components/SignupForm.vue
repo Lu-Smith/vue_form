2dd840d5570d0d538dd2bfa97e6116bb1d53a762
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-13 10:30:59 +0100

    Updated SignupForm.vue

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -4,14 +4,21 @@
         Email:
     </label>
     <input type="email" required v-model="email">
+  <label>
+        Password:
+    </label>
+    <input type="password" required v-model="password">
   </form>
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            email: ''
+            email: '',
+            password: ''
         }
     }
 
